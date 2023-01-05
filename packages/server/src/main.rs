01436c8cc82f94808e7237772950852db1d0f77b
okjlez <zeljkovranjes@outlook.com>
2023-01-04 23:05:32 -0500

    rocket update

--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, wsdadsorld!");
+#[macro_use] extern crate rocket;
+
+
+#[rocket::main]
+async fn main() {
+    /* 
+    let rocket = rocket::build()
+        .mount("/", account::gets::routes()).manage(oauth2_client)
+            .ignite().await?
+            .launch().await?;
+    */
+            
 }
