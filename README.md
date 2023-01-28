c8bc5c89f81b4c49bdc3bd927ff2db55618f0b85
okjlez <zeljkovranjes@outlook.com>
2023-01-27 20:16:38 -0500

    re-added client might remove in the future.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # blog-rust
 A simple blog system written in rust.
+
+
+This is my first project in Rust. 
+I learned a lot. A lot.
+
+
---
+++ packages/client/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<form action="http://127.0.0.1:8000/api/account/login" method="POST">
+    <label for="fname">Email</label>
+    <input type="text" id="email" name="email"><br><br>
+    <label for="lname">Password:</label>
+    <input type="text" id="password" name="password"><br><br>
+    <input type="submit" value="Submit">
+</form> 
