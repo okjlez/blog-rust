618a08bb0ac98c7141e4b15f013d7776cf63f4df
Zeljko <68818452+okjlez@users.noreply.github.com>
2023-01-29 15:46:03 -0500

    Delete _create_table_accounts.sql
    no need for two create table fields.
    
    Signed-off-by: Zeljko <68818452+okjlez@users.noreply.github.com>

--- packages/server/src/sql/_create_table_accounts.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE accounts (
-    id VARCHAR(255) PRIMARY KEY,
-    username username NOT NULL,
-    email VARCHAR(254) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    password_salt VARCHAR(255) NOT NULL,
-    rank public."Rank" NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-)
